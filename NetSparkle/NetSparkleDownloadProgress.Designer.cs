ee1bb8e91215f67ab41fd9f18ed75a50c0961d86
larrynung <larrynung@hotmail.com>
2013-03-18 17:26:34 +0800

    let download form's icon display with zoom size mode

--- NetSparkle/NetSparkleDownloadProgress.Designer.cs
+++ NetSparkle/NetSparkleDownloadProgress.Designer.cs
@@ -61,8 +61,8 @@
 			// 
 			// imgAppIcon
 			// 
-			resources.ApplyResources(this.imgAppIcon, "imgAppIcon");
 			this.imgAppIcon.Image = global::AppLimit.NetSparkle.Properties.Resources.software_update_available1;
+			resources.ApplyResources(this.imgAppIcon, "imgAppIcon");
 			this.imgAppIcon.Name = "imgAppIcon";
 			this.imgAppIcon.TabStop = false;
 			// 
@@ -81,7 +81,6 @@
 			this.TopMost = true;
 			((System.ComponentModel.ISupportInitialize)(this.imgAppIcon)).EndInit();
 			this.ResumeLayout(false);
-			this.PerformLayout();
 
         }
 
